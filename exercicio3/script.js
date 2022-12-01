239d63d0b3430eb079e8f7954cea7f3f6adcb3b1
Lucasdevbulletproof <[bassolucas1997@gmail.com}>
2022-12-01 18:31:08 -0300

    Resolucao-exercicio123-cond1

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,15 @@
-let nacionalidade //= adicione o prompt aqui
+let nacionalidade = prompt("Digite sua nacionalidade aqui")
+if(nacionalidade === "brasileira"){
+    console.log("Tu é brasileira")
+}else if (nacionalidade === "argentina"){
+    console.log("Tu é argentina")
+}else if (nacionalidade === "uruguaia"){
+    console.log("Tu é uruguaia")
+}else if (nacionalidade === "chilena"){
+    console.log("Tu é chilena")
+}else if (nacionalidade === "colombiana"){
+    console.log("Tu é colombiana")
+}else {
+    console.log("Nacionalidade não encontrada")
+}
+    
